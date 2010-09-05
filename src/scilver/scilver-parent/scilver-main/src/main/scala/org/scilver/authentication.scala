17fe9b80e2b2561d059abd414476f006b130c9ba
eav1986 <eav1986@a51d2afd-877f-04c7-268f-8e0aaff28ba0>
2010-09-05 18:30:25 +0000

    oauth passed

--- src/scilver/scilver-parent/scilver-main/src/main/scala/org/scilver/authentication.scala
+++ src/scilver/scilver-parent/scilver-main/src/main/scala/org/scilver/authentication.scala
@@ -1,23 +1,41 @@
 package org.scilver
 
-import java.lang.String
-import twitter4j.{TwitterFactory, Twitter}
-import twitter4j.http.AuthorizationFactory;
-import org.jdesktop.swingx.auth.LoginService
+import twitter4j.{TwitterException, User, TwitterFactory, Twitter}
+
+import twitter4j.http.AccessToken
+import java.awt.Desktop
+import java.net.URI
+import swing.Dialog
 
 /**
  * @author eav
  * Date: 28.08.2010
  * Time: 12:54:04
  */
-object authentication extends LoginService {
+object authentication {
   var twitter: Twitter = null
+  var user: User = null
+  var accessToken: AccessToken = null
 
-  def authenticate(login: String, pass: Array[Char], server: String): Boolean =
+  def authenticate: Boolean =
   {
-    val tf = new TwitterFactory
-    twitter = tf.getInstance(login, new String(pass));
-    twitter.test
+    twitter = new TwitterFactory().getInstance();
+    twitter.setOAuthConsumer("IESXizMmIy26f1pMVPYlhg", "5k31Fkf9re8koYf4MLGlt7Osrz89dUUa2b3ot99ZM")
+
+    val request = twitter.getOAuthRequestToken
+    Desktop.getDesktop.browse(new URI(request.getAuthorizationURL))
+
+    val pin = Dialog.showInput(message = i18n.tr("Enter pin"), initial = "")
+
+    user = null
+    try {
+      accessToken = twitter.getOAuthAccessToken(request, pin.get)
+      user = twitter.verifyCredentials
+    }
+    catch {
+      case e: TwitterException => println(e.getMessage)
+    }
+    user != null
   }
 }
 
