5dd5dc604f2aa77f3b2c34efbc719520417b111f
eav1986 <eav1986@a51d2afd-877f-04c7-268f-8e0aaff28ba0>
2010-09-07 19:55:39 +0000

    authentication refactor

--- src/scilver/scilver-parent/scilver-main/src/main/scala/org/scilver/authentication.scala
+++ src/scilver/scilver-parent/scilver-main/src/main/scala/org/scilver/authentication.scala
@@ -13,32 +13,35 @@ import swing.Dialog
  * Time: 12:54:04
  */
 object authentication {
-  var twitter: Twitter = null
-  var user: User = null
-  var accessToken: AccessToken = null
-
-  def authenticate: Boolean =
-  {
-    twitter = new TwitterFactory().getInstance();
+  def login = {
+    val twitter = new TwitterFactory().getInstance();
     twitter.setOAuthConsumer("IESXizMmIy26f1pMVPYlhg", "5k31Fkf9re8koYf4MLGlt7Osrz89dUUa2b3ot99ZM")
+    enterPin(twitter)
+  }
 
+  private def enterPin(twitter: Twitter): Credentials = {
     val request = twitter.getOAuthRequestToken
     Desktop.getDesktop.browse(new URI(request.getAuthorizationURL))
 
-    val pin = Dialog.showInput(message = i18n.tr("Enter pin"), initial = "")
-
-    user = null
-    try {
-      accessToken = twitter.getOAuthAccessToken(request, pin.get)
-      user = twitter.verifyCredentials
+    Dialog.showInput(message = i18n.tr("Enter pin"), initial = "") match {
+      case None => enterPin(twitter)
+      case pin: Some[String] =>
+        try {
+          val accessToken = twitter.getOAuthAccessToken(request, pin.get)
+          val user = twitter.verifyCredentials
+          Credentials(null, user, accessToken)
+        }
+        catch {
+          case e: TwitterException => {
+            println(e.getMessage)
+            enterPin(twitter)
+          }
+        }
     }
-    catch {
-      case e: TwitterException => println(e.getMessage)
-    }
-    user != null
   }
 }
 
+case class Credentials(twitter: Twitter, user: User, accessToken: AccessToken)
 
 
 
--- src/scilver/scilver-parent/scilver-main/src/test/scala/test/cases/MainFrameTest.scala
+++ src/scilver/scilver-parent/scilver-main/src/test/scala/test/cases/MainFrameTest.scala
@@ -1,13 +1,13 @@
 package test.cases
 
-import test.env.TestApp
 import org.mockito.Mockito
 import twitter4j.{User, Twitter}
 import Mockito._
 import org.fest.swing.fixture.FrameFixture
 import org.testng.annotations.{Test, BeforeClass}
-import org.scilver.mainFrame
 import java.net.URL
+import twitter4j.http.AccessToken
+import org.scilver.{BasicApp, Credentials, mainFrame}
 
 /**
  * @author eav
@@ -19,12 +19,15 @@ class MainFrameTest {
   def setUp {
     val twitter = mock(classOf[Twitter])
     val user = mock(classOf[User])
+    val accessToken = mock(classOf[AccessToken])
 
     when(twitter.verifyCredentials) thenReturn user
     when(user.getScreenName) thenReturn "eav"
     when(user.getProfileImageURL) thenReturn new URL("http://www.gstatic.com/codesite/ph/images/defaultlogo.png")
 
-    TestApp.startup(twitter, user)
+    new BasicApp {
+      override def login = Credentials(twitter, user, accessToken)
+    }.startup(null)
   }
 
   @Test
